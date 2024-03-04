188afbff75dfab1d329cf468a58020c0f3236e8e
saikat112 <saikatmanna112@gmail.com>
2024-03-05 00:27:51 +0530

    exprience

--- client/src/pages/Home/Experiences.jsx
+++ client/src/pages/Home/Experiences.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import SectionTitle from '../../components/SectionTitle'
-
+import { experiences } from '../../resources/experiences'
 function Experiences() {
+  const [selectedItemIndex, setSelectedItemIndex] = React.useState(0);
   return (
-    <SectionTitle title="Experiences" />
+    <div>
+      <SectionTitle title="Experiences" />
+      <div className='flex py-10'>
+        <div className=' flex flex-col gap-5 border-l-2 border-[#89f7f7]'>
+          {experiences.map((experiences, index) => (
+            <div onClick={() => {
+              setSelectedItemIndex(index);
+            }}
+              className='cursor-pointer'>
+              <h1 className={` text-xl px-5 ${selectedItemIndex === index ? 'text-tertiary border-l-4 border-tertiary' : 'text-white'} `}>{experiences.period}</h1>
+            </div>
+          ))}
+        </div>
+        <div></div>
+      </div>
+    </div>
 
   )
 }
