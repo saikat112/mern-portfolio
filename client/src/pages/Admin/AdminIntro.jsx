584d76a6beffa1f46545d808ed5d6f6a9b803984
saikat112 <saikatmanna112@gmail.com>
2024-03-13 00:40:32 +0530

    dubg

--- client/src/pages/Admin/AdminIntro.jsx
+++ client/src/pages/Admin/AdminIntro.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Input } from 'antd';
+import { useSelector } from 'react-redux';
 
 function AdminIntro() {
+  const { portfolioData } = useSelector((state) => state.root);
   const onFinish = (values) => {
     console.log(values)
   }
   return (
     <div>
-      <Form onFinish={onFinish}>
-        <Form.Item name='welcomeText'>
+      <Form onFinish={onFinish} layout='vertical' initialValues={portfolioData?.intro}>
+        <Form.Item name='welcomeText' label='Welcome Text'>
           <input placeholder='Welcome Text' />
         </Form.Item>
-        <Form.Item name='fastName'>
+        <Form.Item name='fastName' label='Fast Name'>
           <input placeholder='Fast Name' />
         </Form.Item>
-        <Form.Item name='lastName'>
+        <Form.Item name='lastName' label='Last Name'>
           <input placeholder='Last Name' />
         </Form.Item>
-        <Form.Item name='caption'>
+        <Form.Item name='caption' label='Captions'>
           <input placeholder='Captions' />
         </Form.Item>
-        <Form.Item name='description'>
+        <Form.Item name='description' label='Description'>
           <textarea placeholder='Description' />
         </Form.Item>
         <div className="flex justify-end w-full">
-          <button className='px-10 py-2 bg-primary text-white'>Save</button>
+          <button className='px-10 py-2 bg-primary text-white' type='submit'>Save</button>
         </div>
       </Form>
     </div>
--- client/src/pages/Admin/index.jsx
+++ client/src/pages/Admin/index.jsx
@@ -5,15 +5,17 @@ import AdminAbout from './AdminAbout';
 import AdminExperiences from './AdminExperiences';
 import AdminProjects from './AdminProjects';
 import AdminContact from './AdminContact';
-
+import { useSelector } from 'react-redux';
 import { Tabs } from 'antd';
+
 
 const TabPane = Tabs.TabPane;
 function Admin() {
+  const { portfolioData } = useSelector((state) => state.root)
   return (
     <div>
       <Header />
-      <div className='mt-5 p-5'>
+      {portfolioData && <div className='mt-5 p-5'>
         <Tabs defaultActiveKey="1">
           <TabPane tab="Intro" key="1"><AdminIntro /></TabPane>
           <TabPane tab="About" key="2"><AdminAbout /></TabPane>
@@ -22,7 +24,7 @@ function Admin() {
           <TabPane tab="Contact" key="5"><AdminContact /></TabPane>
 
         </Tabs>
-      </div>
+      </div>}
     </div>
 
 
